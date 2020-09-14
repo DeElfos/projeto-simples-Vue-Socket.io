2b0f12b97cb39ed08ef3e45d1e297900e74c3476
DeElfos <santosefilhomarcos@gmail.com>
2020-09-14 12:39:37 -0300

    Anexo do Vue-socket.io e tela inicial para testes

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,40 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
-  </q-page>
+<div>
+    <p v-if="isConnected">Você está conectado!</p>
+    <p>Mensagem do servidor: "{{socketMessage}}"</p>
+    <button @click="testaServidor()">Testar Servidor</button>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+    data() {
+        return {
+            isConnected: false,
+            socketMessage: ''
+        }
+    },
+
+    sockets: {
+        connect() {
+            // Fired when the socket connects.
+            this.isConnected = true;
+        },
+
+        disconnect() {
+            this.isConnected = false;
+        },
+        // Fired when the server sends something on the "messageChannel" channel.
+        messageChannel(data) {
+            this.socketMessage = data
+        }
+    },
+
+    methods: {
+        testaServidor() {
+            // Send the "pingServer" event to the server.
+            this.$socket.emit('testaServer', 'PING!')
+        }
+    }
 }
 </script>
